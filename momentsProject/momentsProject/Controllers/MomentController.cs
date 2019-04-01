84f95be846849c3b543c6660d6d460571386f962
oday ibn bari <oday ibn bari@LAPTOP-4L91E1SC>
2019-04-01 14:43:31 +0300

    לא לקחת

--- momentsProject/momentsProject/Controllers/MomentController.cs
+++ momentsProject/momentsProject/Controllers/MomentController.cs
@@ -200,26 +200,8 @@ namespace momentsProject.Controllers
             ViewData[tab] = "active";
 
         }
-    }
-}
-using Moments.dal;
-using Moments.Models;
-using Moments.ModelView;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-
-namespace Moments.Controllers
-{
-    public class MomentController : Controller
-    {
-        // GET: Moment
-        public ActionResult Index()
-        {
-            return View();
-        }
+    
+        
         public static string getMomentTitle(string invitation)
         {
             List<string> splitted = invitation.Split(' ').ToList<string>();
